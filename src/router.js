3c9fffb9c924a0a8774e208561b46701be7809f6
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-16 18:28:00 +0900

    loginのだいたいのを書いた

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Axios from "./views/Axios.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: "/axios",
       name: "axios",
       component: Axios
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     },
     {
       path: "/about",
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="text" v-model="userName" />
+    <input type="text" v-model="password" />
+    <button @click="login">login</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      serverlink: "https://enghzvca8cgzt.x.pipedream.net/",
+      userName: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      if (this.userName != "" && this.password != "") {
+        const loginData = {
+          username: this.userName,
+          password: this.password
+        };
+        axios.post(this.serverlink, loginData);
+      }
+    }
+  }
+};
+</script>
