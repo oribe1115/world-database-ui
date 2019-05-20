00ebc0ae116277862725a2362e810c758eb7c6c7
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-05-20 19:49:17 +0900

    div構成を変えた

--- src/views/Countries.vue
+++ src/views/Countries.vue
@@ -1,35 +1,37 @@
 <template>
   <div>
-    <div class="dropdown-list">
-      <div class="top-list" @click="changeShowRegionList()">
-        <div class="triangle">
-          <div v-if="showRegionList">
-            <font-awesome-icon icon="caret-up" />
+    <div class="body">
+      <div class="dropdown-list">
+        <div class="top-list" @click="changeShowRegionList()">
+          <div class="triangle">
+            <div v-if="showRegionList">
+              <font-awesome-icon icon="caret-up" />
+            </div>
+            <div v-else>
+              <font-awesome-icon icon="caret-down" />
+            </div>
           </div>
-          <div v-else>
-            <font-awesome-icon icon="caret-down" />
+          <div class="now-region">{{ region }}</div>
+        </div>
+        <div v-if="showRegionList">
+          <div class="list-item list-all" @click="changeRegionToAll()">
+            ALL
+          </div>
+          <div v-for="region in regionList" :key="region.region">
+            <div class="list-item" @click="changeShowRegion(region)">
+              {{ region.region }}
+            </div>
           </div>
         </div>
-        <div class="now-region">{{ region }}</div>
       </div>
-      <div v-if="showRegionList">
-        <div class="list-item list-all" @click="changeRegionToAll()">
-          ALL
-        </div>
-        <div v-for="region in regionList" :key="region.region">
-          <div class="list-item" @click="changeShowRegion(region)">
-            {{ region.region }}
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="main">
-      <div v-if="allCountriesName">
-        <div v-for="country in allCountriesName" :key="country.name">
-          <div v-if="judgeForShow(country)">
-            <router-link v-bind:to="/citiesInThisCountry/ + country.code">
-              {{ country.name }}
-            </router-link>
+      <div class="main">
+        <div v-if="allCountriesName">
+          <div v-for="country in allCountriesName" :key="country.name">
+            <div v-if="judgeForShow(country)">
+              <router-link v-bind:to="/citiesInThisCountry/ + country.code">
+                {{ country.name }}
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
